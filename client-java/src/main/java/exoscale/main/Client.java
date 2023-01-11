110d6e84697db28901d1366a6d2a5facdf43113c
Aurélien Deroide <aurelien.deroide@exoscale.ch>
2023-01-11 11:31:53 +0100

    Change java client to show the issue

--- client-java/src/main/java/exoscale/main/Client.java
+++ client-java/src/main/java/exoscale/main/Client.java
@@ -5,6 +5,9 @@ import exoscale.sos.HelloReply;
 import exoscale.sos.HelloRequest;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import java.lang.Thread ;
+
+
 
 // https://grpc.io/docs/languages/java/basics/#client
 public class Client {
@@ -16,12 +19,18 @@ public class Client {
         GreeterGrpc.GreeterBlockingStub stub =
             GreeterGrpc.newBlockingStub(channel);
 
+	System.out.println("Starting RPC");
         HelloReply helloResponse = stub.sayHello(
             HelloRequest.newBuilder()
                 .setName("Ray")
                 .build());
 
         System.out.println(helloResponse);
+	try {
+	    Thread.sleep(10*1000, 0); //10s 
+	} catch (InterruptedException e) {
+	}
+	System.out.println("Shutting down the client channel");
         channel.shutdown();
     }
 }
